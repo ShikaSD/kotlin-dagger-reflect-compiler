b6c9f9fc6cb5a4d5456ec70aa0f887ed47dcd786
Andrei Shikov <andrei.shikov@magiclab.co>
2020-04-20 01:35:38 +0100

    Fix incremental compilation

--- compiler/compiler-plugin/src/main/kotlin/me/shika/dagger/reflect/resolver/DaggerReflectAnalysisHandler.kt
+++ compiler/compiler-plugin/src/main/kotlin/me/shika/dagger/reflect/resolver/DaggerReflectAnalysisHandler.kt
@@ -2,7 +2,10 @@ package me.shika.dagger.reflect.resolver
 
 import me.shika.dagger.reflect.renderer.DaggerReflectRenderer
 import org.jetbrains.kotlin.analyzer.AnalysisResult
+import org.jetbrains.kotlin.com.intellij.openapi.fileEditor.FileDocumentManager
 import org.jetbrains.kotlin.com.intellij.openapi.project.Project
+import org.jetbrains.kotlin.com.intellij.psi.PsiManager
+import org.jetbrains.kotlin.com.intellij.psi.impl.PsiManagerImpl
 import org.jetbrains.kotlin.container.ComponentProvider
 import org.jetbrains.kotlin.container.get
 import org.jetbrains.kotlin.context.ProjectContext
@@ -16,9 +19,16 @@ import org.jetbrains.kotlin.resolve.jvm.extensions.AnalysisHandlerExtension
 import org.jetbrains.kotlin.resolve.lazy.ResolveSession
 import org.jetbrains.kotlin.resolve.scopes.DescriptorKindFilter.Companion.CLASSIFIERS
 import java.io.File
+import java.util.logging.FileHandler
+import java.util.logging.Logger
 
 class DaggerReflectAnalysisHandler(private val outputDir: File) : AnalysisHandlerExtension {
     private var generated = false
+    private val log = Logger.getLogger("test")
+
+    init {
+        log.addHandler(handler)
+    }
 
     override fun doAnalysis(
         project: Project,
@@ -28,6 +38,7 @@ class DaggerReflectAnalysisHandler(private val outputDir: File) : AnalysisHandle
         bindingTrace: BindingTrace,
         componentProvider: ComponentProvider
     ): AnalysisResult? {
+        log.info("Files $files, generated=$generated")
         if (generated) {
             generated = false
             return null
@@ -35,6 +46,8 @@ class DaggerReflectAnalysisHandler(private val outputDir: File) : AnalysisHandle
 
         outputDir.deleteRecursively()
         outputDir.mkdirs()
+
+        project.removeFilesFromLastCompilation(files as MutableCollection<KtFile>)
 
         val resolveSession = componentProvider.get<ResolveSession>()
 
@@ -88,9 +101,31 @@ class DaggerReflectAnalysisHandler(private val outputDir: File) : AnalysisHandle
         renderer.generateDefaultBuilder()
     }
 
+    override fun analysisCompleted(project: Project, module: ModuleDescriptor, bindingTrace: BindingTrace, files: Collection<KtFile>): AnalysisResult? {
+        handler.close()
+        return super.analysisCompleted(project, module, bindingTrace, files)
+    }
+
+    private fun Project.removeFilesFromLastCompilation(files: MutableCollection<KtFile>) {
+        files.removeIf { file ->
+            file.virtualFilePath.startsWith(outputDir.absolutePath).also { removed ->
+                if (removed) {
+                    dropFileCaches(file)
+                }
+            }
+        }
+    }
+
+    private fun Project.dropFileCaches(file: KtFile) {
+        (PsiManager.getInstance(this) as PsiManagerImpl).fileManager.setViewProvider(file.virtualFile, null)
+        file.viewProvider.document?.let { FileDocumentManager.getInstance().reloadFromDisk(it) }
+    }
+
     companion object {
         val COMPONENT_FQ_NAME = FqName(dagger.Component::class.qualifiedName!!)
         val COMPONENT_FACTORY_FQ_NAME = FqName(dagger.Component.Factory::class.qualifiedName!!)
         val COMPONENT_BUILDER_FQ_NAME = FqName(dagger.Component.Builder::class.qualifiedName!!)
+
+        private val handler = FileHandler("/Users/andreishikov/projects/test/dagger-reflect-compiler/test.log", true)
     }
 }
